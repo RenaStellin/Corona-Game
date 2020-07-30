420dec1e9ba8c5ec4051aba746cf7257217772cb
krishna177 <krishns.van@gmail.com>
2020-07-30 22:45:14 +0530

    Krishna
    started with timer not finished

--- Assets/Script/Hospital.cs
+++ Assets/Script/Hospital.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class Hospital : MonoBehaviour
 {
     // Start is called before the first frame update
+    public GameObject effect;
     void Start()
     {
         
@@ -19,6 +21,7 @@ public class Hospital : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collider)
     {
         Debug.Log("Hospital");
+        Instantiate(effect);
 
     }
 }
